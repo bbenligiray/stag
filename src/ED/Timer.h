fa132ec29e387bba2f0bbdde16654f558678312c
wendongcheng <wendongcheng@bytedance.com>
2021-03-15 12:41:49 +0800

    build_for_macos

--- src/ED/Timer.h
+++ src/ED/Timer.h
@@ -1,6 +1,10 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#define BUILD_MACOS
+
+#ifndef BUILD_MACOS
+
 #include <windows.h>
 
 class Timer {
@@ -8,26 +12,60 @@ private:
   __int64 freq, tStart, tStop;
 
 public:
-  Timer(){
+  Timer() {
     // Get the frequency of the hi-res timer
-    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
-  } //end-TimerClass
+    QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+  } // end-TimerClass
 
-  void Start(){
+  void Start() {
     // Use hi-res timer
-    QueryPerformanceCounter((LARGE_INTEGER*)&tStart);
-  } //end-Start
+    QueryPerformanceCounter((LARGE_INTEGER *)&tStart);
+  } // end-Start
 
-  void Stop(){
+  void Stop() {
     // Perform operations that require timing
-    QueryPerformanceCounter((LARGE_INTEGER*)&tStop);
-  } //end-Stop
+    QueryPerformanceCounter((LARGE_INTEGER *)&tStop);
+  } // end-Stop
 
   // Returns time in milliseconds
-  double ElapsedTime(){
+  double ElapsedTime() {
     // Calculate time difference in milliseconds
-    return ((double)(tStop - tStart)/(double)freq)*1e3;
-  } //end-Elapsed
+    return ((double)(tStop - tStart) / (double)freq) * 1e3;
+  } // end-Elapsed
 };
+#else
+
+#include <chrono>
+#include <ctime>
+#include <sys/_types/_int64_t.h>
+#include <sys/_types/_timespec.h>
+
+class Timer {
+private:
+  std::chrono::steady_clock::time_point testTime_t1, testTime_t2;
+
+public:
+  Timer() {} // end-TimerClass
+
+  void Start() {
+    // Use hi-res timer
+    testTime_t1 = std::chrono::steady_clock::now();
+  } // end-Start
+
+  void Stop() {
+    // Perform operations that require timing
+    testTime_t2 = std::chrono::steady_clock::now();
+  } // end-Stop
+
+  // Returns time in milliseconds
+  double ElapsedTime() {
+    // Calculate time difference in milliseconds
+    return std::chrono::duration_cast<std::chrono::duration<double>>(
+               testTime_t2 - testTime_t1)
+        .count();
+  } // end-Elapsed
+};
+
+#endif // !1
 
 #endif
